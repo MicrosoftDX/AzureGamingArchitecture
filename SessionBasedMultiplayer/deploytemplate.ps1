dadba260e1042b15653f605d324183ab1bcc4f6e
rachelyehe <rachelyehe@hotmail.com>
2017-02-22 18:23:17 +0200

    Adding session based multiplayer architecture

---
+++ SessionBasedMultiplayer/deploytemplate.ps1
@@ -0,0 +1,22 @@
+Login-AzureRmAccount
+
+$SubscriptionID = "<YOUR SUBSCRIPTION ID>"
+Select-AzureRmSubscription -SubscriptionId $SubscriptionID
+
+$RGName = "MultiplayerRG"
+$location = "westeurope"
+$templatefile = "azuredeploy.json"
+$templateparameters = "azuredeploy.parameters.json"
+
+
+
+# Create a Resource Group
+New-AzureRmResourceGroup -Name $RGName -Location $location
+ 
+# Deploy the Template to the Resource Group
+New-AzureRmResourceGroupDeployment -ResourceGroupName $RGName `
+                                   -TemplateFile $templatefile `
+                                   -TemplateParameterFile $templateparameters
+
+#Remove-AzureRmResourceGroup $RGName
+                                
